59b6994e89f284c6df213bc7e939701bdf3dccd2
Ed Clements <ed.clements@gmail.com>
2018-10-21 19:29:08 +0100

    Rename to hedaleth

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-cd pkg/hedalth-base
+cd pkg/hedaleth-base
 extra-x86_64-build -c
 cd ../..
-repo-add hedalth.db.tar.xz pkg/hedalth-base/hedalth-base-1-1-x86_64.pkg.tar.xz
+repo-add hedaleth.db.tar.xz pkg/hedaleth-base/hedaleth-base-1-1-x86_64.pkg.tar.xz
 cd pkg/dwm
 extra-x86_64-build -c
 cd ../..
-repo-add hedalth.db.tar.xz pkg/dwm/dwm-6.1-4-x86_64.pkg.tar.xz
-s3cmd sync --acl-public pkg/hedalth-base/hedalth-base-1-1-x86_64.pkg.tar.xz s3://arch.hedalth.net
-s3cmd sync --acl-public pkg/dwm/dwm-6.1-4-x86_64.pkg.tar.xz s3://arch.hedalth.net
-s3cmd sync --acl-public hedalth.*.tar.xz s3://arch.hedalth.net
-s3cmd sync --acl-public index.html s3://arch.hedalth.net
+repo-add hedaleth.db.tar.xz pkg/dwm/dwm-6.1-4-x86_64.pkg.tar.xz
+s3cmd sync --acl-public pkg/hedaleth-base/hedaleth-base-1-1-x86_64.pkg.tar.xz s3://arch.hedaleth.net
+s3cmd sync --acl-public pkg/dwm/dwm-6.1-4-x86_64.pkg.tar.xz s3://arch.hedaleth.net
+s3cmd sync --acl-public hedaleth.*.tar.xz s3://arch.hedaleth.net
+s3cmd sync --acl-public index.html s3://arch.hedaleth.net
