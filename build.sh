272a9f25eafc6b18357831230201f95306ce7f58
Ed Clements <ed.clements@gmail.com>
2019-03-24 11:45:43 +0000

    Explicitly sync files

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ cd ../..
 repo-add hedaleth.db.tar.xz pkg/dwm/dwm-6.1-4-x86_64.pkg.tar.xz
 s3cmd sync --acl-public pkg/hedaleth-base/hedaleth-base-1-1-x86_64.pkg.tar.xz s3://arch.hedaleth.net
 s3cmd sync --acl-public pkg/dwm/dwm-6.1-4-x86_64.pkg.tar.xz s3://arch.hedaleth.net
-s3cmd sync --acl-public hedaleth.*.tar.xz s3://arch.hedaleth.net
+s3cmd sync --acl-public hedaleth.db.tar.xz s3://arch.hedaleth.net/hedaleth.db
+s3cmd sync --acl-public hedaleth.files.tar.xz s3://arch.hedaleth.net/hedaleth.files
 s3cmd sync --acl-public index.html s3://arch.hedaleth.net
